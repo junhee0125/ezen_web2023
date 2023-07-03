293ea04e5712a2e3b84eaab1314c521039bc64ed
504 <504@DESKTOP-6G8G34M>
2023-07-03 13:08:16 +0900

    0703

--- frontend/public/js/0_과제/3_과제/가계부.js
+++ frontend/public/js/0_과제/3_과제/가계부.js
@@ -16,89 +16,129 @@ ex) 입력 예시 [ input 3개 사용 ]
    금액 : <input type="text" />
 
 ex) 출력 예시 [ table 사용 ]
-   날짜         항목      금액         비고
-   2023-06-06   콜라      300         삭제버튼
-   2023-06-05   노트북   1,200,000   삭제버튼
-   2023-06-03   교통비   1,750      삭제버튼
+   날짜         	항목      금액        	  비고
+   2023-06-06   콜라      300        	삭제버튼
+   2023-06-05   노트북   1,200,000   	삭제버튼
+   2023-06-03   교통비   1,750      	삭제버튼
       -총합계-         1,202,050      
  */
-
+console.log('JS파일 연결 성공!!')
 //1.변수선언 
 let date =[];
 let item =[];
 let cost= [];
-let tableHead =''
-let addItem=''
-
+/*
+	
+ */
 function save(){
 	
+	let sum = 0;
+
 	let input_date = document.querySelector('.date').value;
 	let input_item = document.querySelector('.item').value;
 	let input_cost = document.querySelector('.cost').value
-	// 입력받은 데이터에 값이 없는 경우 return;
+	// 공란체크; return;
 	if(input_date =='') {alert('날짜를 입력하세요');return; }
-	if(input_item =='') {alert('날짜를 입력하세요');return; }
-	if(input_cost =='') {alert('날짜를 입력하세요');return; }
+	if(input_item =='') {alert('항목을 입력하세요');return; }
+	if(input_cost =='') {alert('금액을 입력하세요');return; }
 	
 	//각각의 데이터를 배열에 저장
 	date.push(document.querySelector('.date').value); //input박스(class = date)에서 입력받은 데이터 저장
 	item.push(document.querySelector('.item').value); //input박스(class = item)에서 입력받은 데이터 저장
 	cost.push(document.querySelector('.cost').value); //input박스(class = cost)에서 입력받은 데이터 저장
 	
+	//리스트로 출력하기
+	list();
+	
+	//배열저장후 input박스를 공란으로 초기화해주기
+	document.querySelector('.date').value = '';
+	document.querySelector('.item').value = '';
+	document.querySelector('.cost').value = '';
 	
 	console.log(date);
 	console.log(item);
 	console.log(cost);
-	//list_add 함수호출
-	list_add(input_date,input_item,input_cost);
-	//첫번째 데이터가 저장되면 테이블의 헤드를 추가한다.
-	if(date.length == 1){
-		tableHead = `<table class="account_list">
-						<tr>
-							<th>날짜</th>
-							<th>항목</th>
-							<th>금액</th>
-							<th>삭제</th>
-						</tr>`
-						
-		document.querySelector('.table_head').innerHTML = tableHead;
-	}
-	
+
 }
 
-function list_add(d,i,c){
+function list(){
+	let addItem=''
 	
-	console.log("date  : "+d);
-	console.log("item  : "+i);
-	console.log("cost  : "+c);
+	addItem = `<tr>
+				<th>날짜</th>
+				<th>항목</th>
+				<th>금액</th>
+				<th>삭제</th>
+			</tr>`
+	document.querySelector('.account_list').innerHTML = addItem;		
 	
+	for(let i=0 ; i< cost.length ; i++){
+		addItem +=
+				`<tr>
+					<th>${date[i]}</th>
+					<th>${item[i]}</th>
+					<th>${cost[i]}</th>
+					<th>
+						<button onclick = "del(${i})" type="button">삭제하기</button>
+						<button onclick = "update_check(${i})" type="button">수정하기</button>
+					</th>
+				</tr>`
 	
-	//cost = > 천단위 쉼표 출력하기 toLocaleString()
-	
-	 //html추가
-	 addItem += `<tr>
-					<td>${d}</td>
-					<td>${i}</td>
-					<td>${parseInt(c).toLocaleString()}</td>
-					<td><button onclick = "" type="button">삭제하기</button></td>
-				 </tr>`
-	document.querySelector('.account_list').innerHTML += addItem;
-	
-	//총합계 row 추가
+	}
+	document.querySelector('.account_list').innerHTML = addItem;		
+	// 저장된 배열만큼 리스트를 추가했으면. 총합계를 구하는 sum()함수를 호출해서 toLocaleString (천단위 쉼표)적용해서 저장함.
 	sum();
+		addItem += `<tr>
+					<td colspan="2"> -총합계- </td>
+					<td colspan="2">${sum()}</td>
+				 </tr>`
+	document.querySelector('.account_list').innerHTML = addItem;		
 	
 }
 
 function sum(){
-	let sum = 0 ;
+	let sum = 0 // 가계부 총합계 금액 담을 변수
 	
-	for(let i =0 ; i < cost.length ; i++) {
+	//배열의 갯수만큼 반복해서 더하면서 sum 값 구하기
+	for(let i=0 ; i< cost.length ; i++){
+		
+		//cost배열에 저장된 데이터를 인덱스 0부터 꺼내서 Number로 형변환후 더해주기
 		sum += Number(cost[i])
 		
 	}
-	addHtml = `<tr>
-					<td colspan="2"> -총합계- </td>
-					<td colspan="2">${sum.toLocaleString()}</td>
-				 </tr>`
-	document.querySelector('.table_head').innerHTML = addHtml;
+	//
+	sum = sum.toLocaleString()
+	console.log('sum'+sum)
+	//sum 함수를 호출하면 sum변수의 반값을  return;
+	return sum;
+}
+
+
+function del(index){
+	console.log(index);
+	//인수로 가져온 index로 배열에서 삭제한다.
+	
+	date.splice(index,1);
+	item.splice(index,1);
+	cost.splice(index,1);
+	
+	//리스트를 다시 출력한다.(업데이트하기위해서)
+	list();
+	
+}
+
+//수정하기 버튼이 온클릭되면 해당 인덱스 가져온다.
+//인덱스 해당 데이터를 input 박스 value데이터로 넣어준다.
+//수정하기 버튼을 누르면 push해주고 리스트 함수를 호출한다.
+function update_check(index){
+	console.log(`수정할 인덱스 : ${index}`)	
+//	let updateHtml 	= `날짜 : <input class="date" type="date" value=${date[index]}>
+//						항목 : <input class="item" type="text" value=${item[index]}>
+//						금액 : <input class="cost" type="text" value=${cost[index]}>
+//						<button onclick="update()" type="button">수정하기</button>`
+//					  
+//	document.querySelector('.input_box').innerHTML = updateHtml;	
+document.querySelector('.date').value = date[index];	
+document.querySelector('.item').value = item[index];	
+document.querySelector('.cost').value = cost[index];	
 }
