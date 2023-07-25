07e9c8ae4c859870847622d6dcb85d788b2306eb
504 <504@DESKTOP-6G8G34M>
2023-07-25 10:29:43 +0900

    복습1

--- backend/src/과제/과제5/과제5_키오스크.java
+++ backend/src/과제/과제5/과제5_키오스크.java
@@ -64,15 +64,15 @@ public class 과제5_키오스크 {
 		 while(true) {
 			 
 			
-			 int 콜라재고	= Integer.parseInt((재고관리.split("\n")[0]).split(",")[0]);
-			 int 콜라바구니	= Integer.parseInt((재고관리.split("\n")[0]).split(",")[1]);  
-			 int 콜라가격 	= Integer.parseInt((재고관리.split("\n")[0]).split(",")[2]);  		 
-			 int 환타재고 	= Integer.parseInt((재고관리.split("\n")[1]).split(",")[0]); 
-			 int 환타바구니	= Integer.parseInt((재고관리.split("\n")[1]).split(",")[1]);  
-			 int 환타가격 	= Integer.parseInt((재고관리.split("\n")[1]).split(",")[2]);  		 
-			 int 사이다재고	= Integer.parseInt((재고관리.split("\n")[2]).split(",")[0]);  
+			 int 콜라재고	  = Integer.parseInt((재고관리.split("\n")[0]).split(",")[0]);
+			 int 콜라바구니  = Integer.parseInt((재고관리.split("\n")[0]).split(",")[1]);  
+			 int 콜라가격   = Integer.parseInt((재고관리.split("\n")[0]).split(",")[2]);  		 
+			 int 환타재고 	  = Integer.parseInt((재고관리.split("\n")[1]).split(",")[0]); 
+			 int 환타바구니 = Integer.parseInt((재고관리.split("\n")[1]).split(",")[1]);  
+			 int 환타가격 	 = Integer.parseInt((재고관리.split("\n")[1]).split(",")[2]);  		 
+			 int 사이다재고 = Integer.parseInt((재고관리.split("\n")[2]).split(",")[0]);  
 			 int 사이다바구니= Integer.parseInt((재고관리.split("\n")[2]).split(",")[1]);  
-			 int 사이다가격	= Integer.parseInt((재고관리.split("\n")[2]).split(",")[2]);  
+			 int 사이다가격 = Integer.parseInt((재고관리.split("\n")[2]).split(",")[2]);  
 			 
 			 System.out.println("\n\n-------------------- 메뉴 ------------------- ");
 			 System.out.println("1.콜라[300] 2.환타[200] 3.사이다[100] 4.결제");
--- backend/src/복습/part1/내일꺼연습.java
+++ backend/src/복습/part1/내일꺼연습.java
@@ -6,7 +6,9 @@ public class 내일꺼연습 {
 
 	public static void main(String[] args) {
 	
-		
+		String stock = "10,0,300\n10,0,200\n10,0,100\n";
+		int cokeStock
+	;
 				
 		
 	}
