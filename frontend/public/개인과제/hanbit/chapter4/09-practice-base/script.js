8262eab100357cbece50684bd3592db8ebe4edbe
zecace <zecace@DESKTOP-U419MDP>
2023-07-28 08:35:44 +0900

    0728

--- frontend/public/개인과제/hanbit/chapter4/09-practice-base/script.js
+++ frontend/public/개인과제/hanbit/chapter4/09-practice-base/script.js
@@ -3,3 +3,18 @@
 - 원 그래프 옵션에는 폭 100%, 높이 300px 로 설정
 */
 
+/*
+PIE CHART
+================================================ */
+var pieData = {
+  labels: ['고양이', '개', '기린', '그외'],
+  series: [35, 30, 20, 15]
+};
+
+var pieOptions = {
+  width: '100%',
+  height: '300px'
+};
+
+new Chartist.Pie('.animal-chart', pieData, pieOptions);
+
