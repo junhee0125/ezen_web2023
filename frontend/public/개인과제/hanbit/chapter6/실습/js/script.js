a88a18b42cdfcd24fb672b5a66b5056dd085e86d
zecace <zecace@DESKTOP-U419MDP>
2023-08-02 07:44:03 +0900

    0802

--- frontend/public/개인과제/hanbit/chapter6/실습/js/script.js
+++ frontend/public/개인과제/hanbit/chapter6/실습/js/script.js
@@ -2,3 +2,16 @@
  *
  */
 console.log("js파일 연결");
+new LuminousGallery(document.querySelectorAll('.grid-gallery'), {}, {
+	caption: function(trigger) {
+	    return trigger.querySelector('img').getAttribute('alt');
+	}
+});
+//스크립트 추가
+//라이트박스 적용할 요소 지정해주기
+//grid-gallery클래스가 움직이도록 설정
+
+//AOS 애니메이션
+AOS.init({
+  duration: 1000 // 실행시간 늦추기
+});
