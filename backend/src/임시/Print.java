60716f799ab2ccdb3ed1b738bdbb0dffc1df9558
504 <504@DESKTOP-PI60H60>
2023-08-09 17:06:39 +0900

    a

--- backend/src/임시/Print.java
+++ backend/src/임시/Print.java
@@ -11,7 +11,7 @@ public class Print {
 		// 0번 선택
 		System.out.println("\n\n");
 		System.out.println("-------------------------------------");
-		System.out.println("| 0.관리자 모드 | 1. 게임시작 | 2. 게임설명 |");
+		System.out.println("| 관리자 아이디 :  |");
 		System.out.println("-------------------------------------");
 		System.out.println(" 메뉴번호를 입력하세요. >>");
 	}
