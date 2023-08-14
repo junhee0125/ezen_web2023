b41a23b9fbbe0b28b8289f1c9c52732d54a6392b
504 <504@DESKTOP-PI60H60>
2023-08-14 10:30:09 +0900

    0814

--- frontEndProject/src/main/webapp/개인과제/baemin/js/javascript.js
+++ frontEndProject/src/main/webapp/개인과제/baemin/js/javascript.js
@@ -2,7 +2,15 @@
  *
  */
 console.log("JS연결");
-
+/*
+	휠 이벤트의  deltaY(수직스크롤)값을 읽어와서
+	값이 양의 값이면 스크롤이 아래방향으로 1개씩 증가하되
+	(음의 값인경우  페이지는 1씩 감소해서 스크롤이 올라갈수 있게함)
+	디바이스의 innerHeight 만큼 
+	부드럽게 스크롤되도록 하는 함수
+	한번 휠을 작동하면 100ms뒤에 isWheeling을 false로 변경해서
+	휠이 여러번 적용되더라도 한번으로 인식할 수 있도록 함
+ */
 let i = 1; // 현재 페이지 초기값 설정
 let isWheeling = false;
 
@@ -40,3 +48,5 @@ function handleWheel(event) {
 
 	// wheel 이벤트 리스너 등록
 window.addEventListener('wheel', handleWheel);
+
+// # 1 백그라운드 이미지 랜덤하게 가져오기
