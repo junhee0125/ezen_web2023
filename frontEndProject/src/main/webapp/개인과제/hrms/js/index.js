f64ec6e431481d07cb586ac98a39c72161f14dcd
junhee0125 <minervajun@gmail.com>
2023-10-27 18:08:14 +0900

    dddd

---
+++ frontEndProject/src/main/webapp/개인과제/hrms/js/index.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+console.log('연결');
+let atState = false; //출근상태 기본값 
+function changeState(){
+	
+	if(atState == false) {
+		
+		if(confirm('출근하시나요?')) {
+			atState = true;
+			document.querySelector('attendence').innerHTML = `<i class="fa-solid fa-arrow-right-from-bracket fa-rotate-180"></i>퇴 근`;
+			alert('출근처리 되었습니다.');
+			
+		}
+	} else if(atState == true) {
+		if(confirm('퇴근하시나요?')) {
+			atState = false;
+			alert('퇴근처리 되었습니다.');
+		}
+	}
+	
+	
+	
+	
+}
